4dffec052062825753d4b629ab5d19178abd3b13
BasualdoAriel <arielbasualdo2014@outlook.com>
2021-07-02 21:46:21 -0300

    actualización de form en index y php

--- enviar2.php
+++ enviar2.php
@@ -13,7 +13,7 @@ $mensaje .= "Su e-mail es: " . $mail . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'arielbasualdo37@gmail.com';
+$para = 'ariher_08@hotmail.com';
 $asunto = 'Mensaje de KALIA';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
