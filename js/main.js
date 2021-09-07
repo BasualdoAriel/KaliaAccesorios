ef613f25683a9f938b36eb414bcc63e82c1f397a
BasualdoAriel <arielbasualdo2014@outlook.com>
2021-09-07 20:46:41 -0300

    Uso de algunas animaciones, upgrade de carrito y estilos con Jquery

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
 
 /* Defino el nodo del carrito */
-const carrito= document.querySelector('.offcanvas-body');
+const carrito= $('.offcanvas-body');
 /* Creo  array para el carrito*/
 let productosCarrito=[];
-
+let contador=0;
 
 /* Esta función agrega el nombre del producto al carrito y lo actualiza*/
 function Agregar(nombreProducto){
-    let producto=document.createElement('p');
-    producto.textContent=`${nombreProducto} X1`;
-    carrito.appendChild(producto);
-    ActualizarCarrito(producto.textContent);
+    /* carrito.append(`<p>${nombreProducto} X1</p>`); */
+    carrito.append(`<div class="row pt-2"><p class="col-8">${nombreProducto} X1</p><button id="btn${contador}"class="btn btn-danger col-4">X</button>`)
+    console.log(contador);
+    contador=contador+1;
+    console.log(contador);
+    ActualizarCarrito(`${nombreProducto}`);
 }
 
 /* Esta función verifica el estado del LocalStorage, si LS es null, devuelve array vacio. Si el LS tiene info la devuelve*/
 function VerificarLocalStorage(){
     let productos;
-
     if(localStorage.getItem('productos')===null){
         productos=[];
     }
@@ -32,32 +33,43 @@ function ActualizarCarrito(producto){
     productosCarrito.push(producto);
 }
 
+function ArmarToast(nombreProducto){
+    $(".toast").append(`<div class="toast-body pt-0">Agregaste al carrito: ${nombreProducto}</div>`);
+    $(".toast").toast('show');
+}
+
 /* Esta función identifica todos los botones de agregar al carrito y comienza el proceso de adhesión al carro */
 function AgregarAlCarrito(){
-    let botones=document.querySelectorAll('.agregarCarrito')
+    
+    let botones=$('.agregarCarrito');
     for(let boton of botones){
         let nombreProducto=boton.parentElement.parentElement.querySelector('.modal-title').textContent;
         boton.onclick=()=>{
             Agregar(nombreProducto);
             localStorage.setItem('productos',productosCarrito)
+            ArmarToast(nombreProducto);
         }
     }
 }
 
-/*Esta función elimina, del HTML, la información relacionada al carrito */
+
+
+/*Esta función elimina, ocualta con una animación los productos y la info del toast. luego los elimina */
 function EliminarCarrito(){
-    while(carrito.firstChild){
-        carrito.removeChild(carrito.firstChild);
-    }
+    $(".offcanvas-body").slideUp('slow',()=>{
+        $(".offcanvas-body").empty();
+        $(".toast").children(".toast-body").remove();
+        $(".offcanvas-body").slideDown('fast');
+    });
 }
 
 /* Esta función vacia el carrito de LS */
 function VaciarCarrito(){
-    let botonVaciar=document.querySelector('.vaciarCarrito');
-    botonVaciar.onclick=()=>{
+    let botonVaciar=$('.vaciarCarrito');
+    botonVaciar.click(function () {
         EliminarCarrito();
         localStorage.clear();
-    }
+      })
 }
 
 /* Esta función recarga la info del carrito, si hay LS, entre las distintas páginas*/
@@ -67,25 +79,43 @@ function RecargarCarrito(){
         let arrayProd;
         productosLs=localStorage.getItem('productos');
         arrayProd=productosLs.split(',');
-        for(let i=0;i<arrayProd.length;i++){
-            let producto=document.createElement('p');
-            producto.textContent=arrayProd[i];
-            carrito.appendChild(producto);
-            ActualizarCarrito(producto.textContent);
+        let i=0
+        for(i;i<arrayProd.length;i++){
+            carrito.append($(`<div class="row pt-2"><p class="col-8">${arrayProd[i]} X1</p><button id="btn${i}"class="btn btn-danger col-4">X</button>`))
+            ActualizarCarrito(arrayProd[i]);
         }
+        contador=contador+i+1;
     }
 }
+
+/* function EliminarProducto(){
+    let productoEliminar=$(".btn-danger");
+
+    for(let producto of productoEliminar){
+        producto.onclick=()=>{
+            let id=productoEliminar.attr('id');
+            $(`#${id}`).parent().remove();
+        }
+    }
+    
+    
+} */
 
 /* Esta función accede al LS*/
 function LeerLS(){
     let productosLS;
     productosLS=VerificarLocalStorage();
     RecargarCarrito(productosLS)
+
 }
 
+
+
 function main(){
+    $(".offcanvas-body").empty();
     AgregarAlCarrito(carrito)
-    document.addEventListener('DOMContentLoaded',LeerLS())
+    $(document).ready(LeerLS());
+    /* EliminarProducto(); */
     VaciarCarrito();
 }   
 
